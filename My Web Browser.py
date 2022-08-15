43fdd3b483cd015120b55ece925aa86aacbe7030
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2022-08-15 17:07:11 +0530

    Update and rename Web Browser.py to My Web Browser.py

--- My Web Browser.py
+++ My Web Browser.py
@@ -1,7 +1,10 @@
-import sys
+# My Browser:
+
+from sys import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -10,43 +13,49 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        #navbar
-        navbar = QToolBar()
-        self.addToolBar(navbar)
+        # Navigation Bar
+        nav_bar = QToolBar()
+        self.addToolBar(nav_bar)
 
         back_btn = QAction("Back", self)
         back_btn.triggered.connect(self.browser.back)
-        navbar.addAction(back_btn)
+        nav_bar.addAction(back_btn)
 
         forward_btn = QAction("Forward", self)
         forward_btn.triggered.connect(self.browser.forward)
-        navbar.addAction(forward_btn)
+        nav_bar.addAction(forward_btn)
 
         reload_btn = QAction("Reload", self)
         reload_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(reload_btn)
+        nav_bar.addAction(reload_btn)
 
         home_btn = QAction("Home", self)
         home_btn.triggered.connect(self.navigate_home)
-        navbar.addAction(home_btn)
+        nav_bar.addAction(home_btn)
 
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
-        navbar.addWidget(self.url_bar)
+        nav_bar.addWidget(self.url_bar)
 
         self.browser.urlChanged.connect(self.update_url)
 
     def navigate_home(self):
         self.browser.setUrl(QUrl("http://google.com"))
 
-    def update_url(self, url):
-        self.url_bar.setText(url.toString())
-
     def navigate_to_url(self):
         url = self.url_bar.text()
+        if url[-1:-5] != "moc.":
+            url = "http://google.com/search?q="+url
         self.browser.setUrl(QUrl(url))
+        if url[:8] != "https://":
+            url = "https://" + self.url_bar.text()
 
-app = QApplication(sys.argv)
-QApplication.setApplicationName('Cool Browser')
-window = MainWindow()
-app.exec()
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+if __name__ == "__main__":
+    app = QApplication(argv)
+    QApplication.setApplicationName("My Browser")
+    window = MainWindow()
+    app.exec()
