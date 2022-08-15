f541b8e473ae8342438384022e8776f719479225
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2022-08-15 13:47:36 +0530

    Update FLAMES Calculator.py

--- FLAMES Calculator.py
+++ FLAMES Calculator.py
@@ -1,15 +1,22 @@
 def flames(n1,n2):
+    n1 = n1.replace(' ','').upper()
+    n2 = n2.replace(' ','').upper()
+
+    if n1 == n2:
+        print("Flames for same names is not valid!")
+        exit(0)
+    s = ""
+    
     for i in n1:
         if i in n2:
-                n1.replace(i,"",1)
-                n2.replace(i,"",1)
+                s += i*2
                 
-    n = len(n1+n2)
+    n = len(n1+n2) - len(s)
     
     F = ["Friends", "Lovers", "Attraction", "Marriage", "Enemies", "Siblings"]
  
     while len(F) > 1 :
-        Index = (n%len(F) - 1) # reference for slicing the list
+        Index = n%len(F) - 1 # reference for slicing the list
         
         if Index >= 0 :
             F = F[Index + 1 : ] + F[ : Index]
@@ -18,11 +25,8 @@ def flames(n1,n2):
 
     print(f"Relationship status : '{F[0]}'")
 
-n1 = input("Enter your name: ").replace(' ','').upper()
-n2 = input("Enter your crush name: ").replace(' ','').upper()
+if __name__ == "__main__":
+    n1 = input("Enter your full name: ")
+    n2 = input("Enter your crush full name: ")
 
-if n1 == n2:
-    print("Flames on same names is not applicable")
-    exit(0)
-
-flames(n1,n2)
+    flames(n1, n2)
